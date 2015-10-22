2dce2536e0fd0a9f3ad4936d2e21669d488d82a7
Marine Guillanton <marine.guillanton@gmail.com>
2015-10-22 14:44:51 -0700

    styling

--- characterMatrix.js
+++ characterMatrix.js
@@ -5,11 +5,21 @@ left->right, right->left and in spirals.
 Do it naively, then use dynamic programming.*/
 
 
+var toggle = function(matrix, x, y) {
+  if (matrix[x][y] === false) {
+    matrix[x][y] = true;
+  } else if (matrix[x][y] === true) {
+    matrix[x][y] = false;
+  }
+};
+
+
 var findWords = function(matrix, dict) {
 
   var sequence = '';
   var wordsObj = {};
-
+  var words = [];
+  
   // copy matrix to keep track of path already taken
   var width = matrix[0].length;
   var height = matrix.length;
@@ -21,22 +31,6 @@ var findWords = function(matrix, dict) {
     }
   }
   
-  var toggle = function(matrix, x, y) {
-    if (matrix[x][y] === false) {
-      matrix[x][y] = true;
-    } else if (matrix[x][y] === true) {
-      matrix[x][y] = false;
-    }
-  };
-
-  var checkSequence = function(seq, dict, obj) {
-    if (seq in dict) {
-      // console.log('found', seq);
-      obj[seq] = 1;
-    }
-    return;
-  };
-
   var goNext = function(seq, path, sense, dir, x, y) {
 
     // determine the direction of the spiral and possible options
@@ -67,14 +61,18 @@ var findWords = function(matrix, dict) {
     } else {
       // if path has not already been taken
       if (path[x][y] === false) {
-        seq += matrix[x][y]; // add character
-        // tries.push(seq);
-        checkSequence(seq, dict, wordsObj);
-        toggle(path, x, y); // add coordinates to path
+        // add character to sequence
+        seq += matrix[x][y];
+        if (seq in dict) {
+          // console.log('found', seq);
+          wordsObj[seq] = 1;
+        }
+        // add coordinates to path
+        toggle(path, x, y);
         // go to next directions
         goNext(seq, path, sense, dir, x, y);
         goNext(seq, path, sense, dir2, x, y);
-        // remove coordinates to path
+        // remove coordinates from path
         toggle(path, x, y);
         // no need to slice sequence
       } else { // if path has already been taken, do nothing
@@ -84,45 +82,34 @@ var findWords = function(matrix, dict) {
     }
     return;
   };
-  
+
   // call goNext on each cell of the matrix in both sense of rotation
   for (var h = 0; h < height; h++) {
     for (var w = 0; w < width; w++) {
       sequence = matrix[h][w];
-      checkSequence(sequence, dict, wordsObj);
+      if (sequence in dict) {
+        // console.log('found', seq);
+        wordsObj[sequence] = 1;
+      }
       toggle(path, h, w);
       // first move in any possible direction
-      // tries.push('......clock.......');
-      // tries.push('>>>>>>>>>>>>>');
       goNext(sequence, path, 'clock', 'right', h, w);
-      // tries.push('!!!!!!!!!!!!!!');
       goNext(sequence, path, 'clock', 'down', h, w);
-      // tries.push('<<<<<<<<<<<<<<');
       goNext(sequence, path, 'clock', 'left', h, w);
-      // tries.push('^^^^^^^^^^^^^^');
       goNext(sequence, path, 'clock', 'up', h, w);
-      // tries.push('......reverse.......');
-      // tries.push('>>>>>>>>>>>>>');
       goNext(sequence, path, 'reverse', 'right', h, w);
-      // tries.push('!!!!!!!!!!!!!!');
       goNext(sequence, path, 'reverse', 'down', h, w);
-      // tries.push('<<<<<<<<<<<<<<');
       goNext(sequence, path, 'reverse', 'left', h, w);
-      // tries.push('^^^^^^^^^^^^^^');
       goNext(sequence, path, 'reverse', 'up', h, w);
       toggle(path, h, w);
     }
   }
+  
+  for (var key in wordsObj) {
+    words.push(key);
+  }
 
-
-  var returnWords = function(obj) {
-    var words = [];
-    for (var key in obj) {
-      words.push(key);
-    }
-    return words;
-  };
-  return returnWords(wordsObj);
+  return words;
 };
 
 var input = [['b', 'c', 'a'], ['o', 'a', 't'], ['o', 'c', 's'], ['b', 'i', 'e']];
@@ -130,6 +117,6 @@ var input = [['b', 'c', 'a'], ['o', 'a', 't'], ['o', 'c', 's'], ['b', 'i', 'e']]
 // ['o', 'a', 't']
 // ['o', 'c', 's']
 // ['b', 'i', 'e']
-var dictionary = {bicycle: 1, boat:1, boats:1, boobie:1, boobies:1, cat:1, cats:1, cube:1, flowers:1, friend:1, glass:1, hat:1, oat:1};
+var dictionary = {bicycle: 1, boat:1, boats:1, boob:1, boobie:1, boobies:1, cat:1, cats:1, cube:1, flowers:1, friend:1, glass:1, hat:1, oat:1};
 // words inside dictionary: cat, cats, boobies, boobie, boat, oat
 console.log(findWords(input, dictionary));
